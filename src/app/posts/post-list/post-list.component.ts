0860cab1744bb09173731c83afd002c73afa3d7c
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-12-05 02:04:10 +1100

    Lecture 28 - 5/12

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -9,13 +10,26 @@ import { PostsService } from '../posts.service';
     styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent {
+export class PostListComponent implements OnInit {
     // posts = [
     //     { title: 'First Post', content: 'This is the first post\'s content' },
     //     { title: 'Second Post', content: 'This is the second post\'s content' },
     //     { title: 'Third Post', content: 'This is the third post\'s content' },
     // ];
-    @Input() posts: Post[] = [];
+    posts: Post[] = [];
+    private postsSub: Subscription;
 
     constructor(public postsService: PostsService) { }
+
+    ngOnInit() {
+        this.posts = this.postsService.getPosts();
+        this.postsService.getPostUpdateListener()
+            .subscribe((posts: Post[]) => {
+                this.posts = posts;
+            });
+    }
+
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
+    }
 }
