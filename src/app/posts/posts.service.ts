5d71a5b143d817e0a4aab56b87095ce70f062190
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-12-05 22:24:16 +1100

    Lesson 56 - 5/12

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -13,11 +14,17 @@ export class PostsService {
 
     getPosts() {
         this.http
-            .get<{ message: string, posts: Post[] }>(
-                "http://localhost:3000/api/posts"
-            )
-            .subscribe(postData => {
-                this.posts = postData.posts;
+            .get<{ message: string, posts: any }>("http://localhost:3000/api/posts")
+            .pipe(map(postData => {
+                return postData.posts.map(post => {
+                    return {
+                        title: post.title,
+                        content: post.content,
+                        id: post._id
+                    };
+                });
+            })).subscribe(transformedPosts => {
+                this.posts = transformedPosts;
                 this.postsUpdated.next([...this.posts]);
             });
     }
@@ -28,8 +35,7 @@ export class PostsService {
 
     addPost(title: string, content: string) {
         const post: Post = { id: null, title: title, content: content };
-        this.http
-            .post<{ message: string }>("http://localhost:3000/api/posts", post)
+        this.http.post<{ message: string }>("http://localhost:3000/api/posts", post)
             .subscribe(responseData => {
                 console.log(responseData.message);
                 this.posts.push(post);
