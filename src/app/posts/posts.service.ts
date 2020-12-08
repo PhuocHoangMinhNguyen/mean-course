87a4c3a9468e899ea6b1ffd84648c3e12b6116bb
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-12-09 01:10:30 +1100

    Lesson 69 - 8/12

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -36,7 +36,9 @@ export class PostsService {
     }
 
     getPost(id: string) {
-        return { ...this.posts.find(p => p.id === id) };
+        return this.http.get<{ _id: string, title: string, content: string }>(
+            BACKEND_URL + "/" + id
+        );
     }
 
     addPost(title: string, content: string) {
@@ -53,7 +55,11 @@ export class PostsService {
     updatePost(id: string, title: string, content: string) {
         const post: Post = { id: id, title: title, content: content };
         this.http.put(BACKEND_URL + "/" + id, post).subscribe(response => {
-            console.log(response);
+            const updatedPosts = [...this.posts];
+            const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+            updatedPosts[oldPostIndex] = post;
+            this.posts = updatedPosts;
+            this.postsUpdated.next([...this.posts]);
         })
     }
 
