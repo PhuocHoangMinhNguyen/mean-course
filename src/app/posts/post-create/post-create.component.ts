0380c186f5da48ba8ee4b85f61397aa0d317ad0b
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2020-12-27 01:00:10 +1100

    Lesson 74 (27/12)

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Post } from './../post.model';
@@ -16,12 +16,23 @@ export class PostCreateComponent implements OnInit {
     enteredContent = '';
     post: Post;
     isLoading = false;
+    form: FormGroup;
     private mode = 'create';
     private postId: string;
 
     constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
     ngOnInit() {
+        this.form = new FormGroup({
+            "title": new FormControl(null, {
+                validators: [
+                    Validators.required,
+                    Validators.minLength(3)
+                ]
+            }),
+            "content": new FormControl(null, { validators: [Validators.required] }),
+
+        });
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
             if (paramMap.has('postId')) {
                 this.mode = 'edit';
@@ -30,6 +41,10 @@ export class PostCreateComponent implements OnInit {
                 this.postsService.getPost(this.postId).subscribe(postData => {
                     this.isLoading = false;
                     this.post = { id: postData._id, title: postData.title, content: postData.content };
+                    this.form.setValue({
+                        "title": this.post.title,
+                        "content": this.post.content
+                    });
                 });
             } else {
                 this.mode = 'create';
@@ -38,16 +53,23 @@ export class PostCreateComponent implements OnInit {
         });
     }
 
-    onSavePost(form: NgForm) {
-        if (form.invalid) {
+    onSavePost() {
+        if (this.form.invalid) {
             return;
         }
         this.isLoading = true;
         if (this.mode === 'create') {
-            this.postsService.addPost(form.value.title, form.value.content);
+            this.postsService.addPost(
+                this.form.value.title,
+                this.form.value.content
+            );
         } else {
-            this.postsService.updatePost(this.postId, form.value.title, form.value.enteredContent)
+            this.postsService.updatePost(
+                this.postId,
+                this.form.value.title,
+                this.form.value.enteredContent
+            );
         }
-        form.resetForm();
+        this.form.reset();
     }
 }
