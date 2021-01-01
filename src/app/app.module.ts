5ed5fd230245e4a9dc191758c48341cc05d247c8
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2021-01-02 01:13:57 +1100

    Update (2/1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AngularMaterialModule } from './angular-material.module';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
-import { AppRoutingModule } from './app-routing.module';
+import { PostsModule } from './posts/posts.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PostCreateComponent,
-    PostListComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
+    AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppRoutingModule,
     AngularMaterialModule,
+    PostsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
