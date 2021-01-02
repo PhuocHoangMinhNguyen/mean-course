b686973871211d1102616e54a263c1f60a38c93d
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2021-01-02 23:10:10 +1100

    Lesson 102 - 2/1

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+
+import { AuthData } from './auth-data.model';
+
+const BACKEND_URL = environment.apiUrl + "/user/";
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+    constructor(private http: HttpClient) { }
+
+    createUser(email: string, password: string) {
+        const authData: AuthData = { email: email, password: password };
+        this.http.post(BACKEND_URL + "signup", authData)
+            .subscribe(response => {
+                console.log(response);
+            })
+    }
+}
