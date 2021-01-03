b2f18e3561d937befbb03d25345fc0dfb4390407
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2021-01-03 19:01:54 +1100

    Lesson 133 - 3/1

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
 import { RouterModule } from '@angular/router';
 
-import { PostListComponent } from './post-list/post-list.component';
 import { PostCreateComponent } from './post-create/post-create.component';
+import { PostListComponent } from './post-list/post-list.component';
 import { AngularMaterialModule } from './../angular-material.module';
 
 @NgModule({
