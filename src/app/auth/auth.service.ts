a47fe0024a8b6008f1862638c0d9553afe8f9f29
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2021-01-03 18:18:37 +1100

    Lesson 122 - 3/1

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -37,8 +37,10 @@ export class AuthService {
 
     createUser(email: string, password: string) {
         const authData: AuthData = { email: email, password: password };
-        this.http.post(BACKEND_URL + "signup", authData).subscribe(response => {
-            console.log(response);
+        return this.http.post(BACKEND_URL + "signup", authData).subscribe(() => {
+            this.router.navigate["/"];
+        }, error => {
+            this.authStatusListener.next(false);
         });
     }
 
@@ -62,6 +64,8 @@ export class AuthService {
                     this.saveAuthData(token, expirationDate, this.userId);
                     this.router.navigate(['/']);
                 }
+            }, error => {
+                this.authStatusListener.next(false);
             });
     }
 
