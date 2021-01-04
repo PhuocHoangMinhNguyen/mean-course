56a144a33c0c68b4d9244dee976f183fd8de7231
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2021-01-05 01:13:56 +1100

    Update (5/1)

--- backend/app.js
+++ backend/app.js
@@ -8,23 +8,23 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-mongoose
-    .connect(
-        "mongodb+srv://max:" +
-        process.env.MONGO_ATLAS_PW +
-        "@cluster0.yt8ea.mongodb.net/node-angular?retryWrites=true&w=majority",
-        { useNewUrlParser: true, useUnifiedTopology: true }
-    ).then(() => {
-        console.log("Connected to database!");
-    })
-    .catch(() => {
-        console.log("Connection failed!");
-    });
+mongoose.connect(
+    "mongodb+srv://max:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0.yt8ea.mongodb.net/node-angular?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => {
+    console.log("Connected to database!");
+}).catch(() => {
+    console.log("Connection failed!");
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join(__dirname, "images")));
-app.use("/", express.static(path.join(__dirname, "angular")));
+
+app.use("/images", express.static(path.join("backend/images")));
+// app.use("/images", express.static(path.join(__dirname, "images")));
+// app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -41,9 +41,8 @@ app.use((req, res, next) => {
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
-
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "angular", "index.html"));
-});
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, "angular", "index.html"));
+// });
 
 module.exports = app;
--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -8,6 +8,7 @@ exports.createPost = (req, res, next) => {
         imagePath: url + "/images/" + req.file.filename,
         creator: req.userData.userId
     });
+    console.log(post.imagePath);
     post
         .save()
         .then(createdPost => {
