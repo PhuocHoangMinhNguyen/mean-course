8abfe723594ab978fd0320de3d43385ead72e4e5
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2021-01-05 22:55:57 +1100

    Update (5/1)

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/images", express.static(path.join(__dirname, "images")));
-// app.use("/", express.static(path.join(__dirname, "angular")));
+app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -40,8 +40,8 @@ app.use((req, res, next) => {
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
-// app.use((req, res, next) => {
-//     res.sendFile(path.join(__dirname, "angular", "index.html"));
-// });
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
